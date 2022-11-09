03ed780a303a01182f5de8f017f238c5e4949ee7
Lythmass <lythmass17@gmail.com>
2022-11-09 20:19:37 +0400

    fix: remove default comments

--- database/migrations/2022_11_09_151931_create_movies_table.php
+++ database/migrations/2022_11_09_151931_create_movies_table.php
@@ -5,11 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
 	public function up()
 	{
 		Schema::create('movies', function (Blueprint $table) {
@@ -20,11 +15,6 @@ return new class extends Migration {
 		});
 	}
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
 	public function down()
 	{
 		Schema::dropIfExists('movies');

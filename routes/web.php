f303b88648b76b9e57c78a910321604901c75625
Lythmass <lythmass17@gmail.com>
2022-11-11 15:50:16 +0400

    feat: add login page
    adding new login page with styled email and password inputs. changing users tables 'name' column to 'username'.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', [MovieController::class, 'index']);
 Route::get('/movie/{movie:slug}', [MovieController::class, 'show']);
+
+Route::get('/login', [UserController::class, 'show']);

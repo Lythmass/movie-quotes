4ac26f4077ec521519d960f55996c76734e15a63
Lythmass <lythmass17@gmail.com>
2022-11-14 13:24:27 +0400

    feat: edit-movie
    add editing page for movies, where you can change the title of the movie. creating new request to validate the input title for the movie.

--- app/Http/Controllers/AdminMovieController.php
+++ app/Http/Controllers/AdminMovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreMovieRequest;
 use App\Models\Movie;
 
 class AdminMovieController extends Controller
@@ -11,6 +12,20 @@ class AdminMovieController extends Controller
 		return view('users.index', [
 			'movies' => Movie::all(),
 		]);
+	}
+
+	public function edit(Movie $movie)
+	{
+		return view('movies.edit', [
+			'movie' => $movie,
+		]);
+	}
+
+	public function update(Movie $movie, StoreMovieRequest $request)
+	{
+		$attributes = $request->validated();
+		$movie->update($attributes);
+		return redirect(route('movies-dashboard'));
 	}
 
 	public function destroy(Movie $movie)
--- resources/views/components/movies-list.blade.php
+++ resources/views/components/movies-list.blade.php
@@ -5,7 +5,7 @@
             <div class = "px-10 flex justify-between">
                 <h3 class = "w-[1rem]">{{ $movie->title }}</h3>
                 <h3>{{ $movie->created_at->diffForHumans() }}</h3>
-                <h3 class = "text-blue-700 hover:text-blue-900"><a href = "#">Edit</a></h3>
+                <h3 class = "text-blue-700 hover:text-blue-900"><a href = "/dashboard/movies/{{$movie->id}}/edit">Edit</a></h3>
                 <form action="/dashboard/movies/{{ $movie->id }}" method="post">
                     @csrf
                     @method('DELETE')

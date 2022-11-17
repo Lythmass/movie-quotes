785628d0fe1df210bbcb228bb15ede6a8083c054
Lythmass <lythmass17@gmail.com>
2022-11-17 16:00:44 +0400

    feat: add language change button
    add two buttons in layout blade, which then changes the locale parameter of the route on click. also fix some design bugs.

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <div class = "flex flex-col w-3/4 h-[35rem] bg-white rounded-xl shadow-lg">
-        <div class = "fixed w-3/4 h-[4rem] py-5 shadow-lg shadow-slate-300 bg-white rounded-t-xl">
+    <div class = "flex flex-col w-[70rem] h-[35rem] bg-white rounded-xl shadow-lg">
+        <div class = "fixed w-[70rem] h-[4rem] py-5 shadow-lg shadow-slate-300 bg-white rounded-t-xl">
             <div class = "flex gap-10 justify-center text-xl">
                 @lang('profile.new-movie')
             </div>

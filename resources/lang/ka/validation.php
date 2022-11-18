accc4d5638a1af535bf57dac81513a356011e84e
Lythmass <lythmass17@gmail.com>
2022-11-18 09:33:14 +0400

    feat: change validation language
    add new validation file in ka folder and add two elements there. also fix some validation bugs.

---
+++ resources/lang/ka/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'min'        => [
+		'string'  => ':attribute უნდა იყოს არანაკლებ :min სიმბოლოს ტოლი.',
+	],
+	'required'             => ':attribute ველის შევსება აუცილებელია.',
+];
--- resources/views/components/movie-input.blade.php
+++ resources/views/components/movie-input.blade.php
@@ -3,7 +3,7 @@
 <label for="{{ $id }}" class = "text-xl">@lang($language)</label>
 @if (strlen($value))
     <input required value = {{ $value }} class = "border border-gray-700" id = "{{ $id }}" name = "{{ $name }}" type="text">
-    @error('{{ $name }}')
+    @error($name)
         <p class="text-red-500">{{ $message }}</p>
     @enderror
 @else

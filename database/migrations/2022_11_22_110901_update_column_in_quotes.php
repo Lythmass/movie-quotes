5598e7e35ae606a68599a318b31e627f9e92a6e6
Lythmass <lythmass17@gmail.com>
2022-11-22 15:37:01 +0400

    fix: update column

--- database/migrations/2022_11_22_110901_update_column_in_quotes.php
+++ database/migrations/2022_11_22_110901_update_column_in_quotes.php
@@ -7,12 +7,8 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
 	public function up()
 	{
-		Schema::create('quotes', function (Blueprint $table) {
-			$table->id();
-			$table->foreignId('movie_id');
-			$table->string('text');
-			$table->string('image');
-			$table->timestamps();
+		Schema::table('quotes', function (Blueprint $table) {
+			$table->foreignId('movie_id')->change()->references('id')->on('movies')->onDelete('cascade');
 		});
 	}
 

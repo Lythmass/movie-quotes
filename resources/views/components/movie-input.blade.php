4532cbc1c52704d2e884a9b083e1dac66e76fd06
Lythmass <lythmass17@gmail.com>
2022-11-24 11:37:07 +0400

    fix: increase input widths

--- resources/views/components/movie-input.blade.php
+++ resources/views/components/movie-input.blade.php
@@ -2,12 +2,12 @@
 
 <label for="{{ $id }}" class = "text-xl">@lang($language)</label>
 @if (strlen($value))
-    <input required value = {{ $value }} class = "border border-gray-700" id = "{{ $id }}" name = "{{ $name }}" type="text">
+    <input required value = {{ $value }} class = "rounded py-1 px-[0.75rem] text-[1.2rem] border border-gray-700" id = "{{ $id }}" name = "{{ $name }}" type="text">
     @error($name)
         <p class="text-red-500">{{ $message }}</p>
     @enderror
 @else
-    <input required value = '' class = "border border-gray-700" id = "{{ $id }}" name = "{{ $name }}" type="text">
+    <input required value = '' class = "rounded py-1 px-[0.75rem] text-[1.2rem] border border-gray-700" id = "{{ $id }}" name = "{{ $name }}" type="text">
     @error($name)
         <p class="text-red-500">{{ $message }}</p>
     @enderror

c822d558368f191ce489f14e50c1591445a8aa2b
Lythmass <lythmass17@gmail.com>
2022-11-27 14:07:35 +0400

    feat: add confirmation to delete

--- resources/views/components/movies-list.blade.php
+++ resources/views/components/movies-list.blade.php
@@ -6,7 +6,7 @@
                 <h3 class = "w-[1rem]">{{ $movie->title }}</h3>
                 <h3>{{ $movie->created_at->diffForHumans() }}</h3>
                 <h3 class = "text-blue-700 hover:text-blue-900"><a href = "{{ route('movies-edit', [app()->getLocale(), $movie->id]) }}">@lang('profile.edit')</a></h3>
-                <form action="{{ route('movies-delete', [app()->getLocale(), $movie->id]) }}" method="post">
+                <form onsubmit = "return confirmDelete('{{$movie->title}}')" action="{{ route('movies-delete', [app()->getLocale(), $movie->id]) }}" method="post">
                     @csrf
                     @method('DELETE')
                     <button class = "text-red-700 hover:text-red-900" type="submit">@lang('profile.delete')</button>
@@ -15,3 +15,9 @@
         @endforeach
     </div>
 </div>
+<script>
+    function confirmDelete(title) {
+        var answer = confirm(`Are you sure you want to delete ${title}?`);
+        return answer == true ? true : false;
+    }
+</script>
--- resources/views/components/quotes-list.blade.php
+++ resources/views/components/quotes-list.blade.php
@@ -6,7 +6,7 @@
                 <h3 class = "w-[1rem] hover:underline"><a href = "{{ route('movie', [app()->getLocale(), $quote->movie->slug]) }}">{{ ucwords($quote->movie->title) }}</a></h3>
                 <h3>{{ $quote->created_at->diffForHumans() }}</h3>
                 <h3 class = "text-blue-700 hover:text-blue-900"><a href = "{{ route('quotes-edit', [app()->getLocale(), 'quote' => $quote->id]) }}">@lang('profile.edit')</a></h3>
-                <form action="{{ route('quotes-delete', [app()->getLocale(), $quote->id]) }}" method="post">
+                <form onsubmit = "return confirmDelete('{{$quote->text}}')" action="{{ route('quotes-delete', [app()->getLocale(), $quote->id]) }}" method="post">
                     @csrf
                     @method('DELETE')
                     <button type = "submit" class = "text-red-700 hover:text-red-900">@lang('profile.delete')</button>
@@ -16,3 +16,9 @@
         @endforeach
     </div>
 </div>
+<script>
+    function confirmDelete(title) {
+        var answer = confirm(`Are you sure you want to delete ${title}?`);
+        return answer == true ? true : false;
+    }
+</script>
